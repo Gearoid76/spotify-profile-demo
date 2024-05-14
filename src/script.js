156f86d346372979fc86fc521024897bb45dae1f
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-05-14 13:35:01 +0300

    working on dropdown menu

--- src/script.js
+++ src/script.js
@@ -41,6 +41,7 @@ if (!code) {
             const options = [
                 { label: 'Option 1', value: 'option1'},
                 { label: 'Option 2', value: 'option2'},
+                { label: 'Option 3', value: 'option3'},
             ];
 
             options.forEach(option => {
